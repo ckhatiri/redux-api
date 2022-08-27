9d1330639a5682237f408d215d766d417bac850c
chahrazad <chahrazadkhatiri.2@gmail.com>
2022-08-27 18:50:11 +0100

    first commit

---
+++ api/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const { json } = require('body-parser');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+let todos = [
+	{
+		id: Date.now(),
+		title: 'todo 1',
+		completed: true,
+	},
+	{
+		id: Date.now(),
+		title: 'todo 2',
+		completed: false,
+	},
+	{
+		id: Date.now(),
+		title: 'todo 3',
+		completed: false,
+	},
+	{
+		id: Date.now(),
+		title: 'todo 4',
+		completed: false,
+	},
+	{
+		id: Date.now(),
+		title: 'todo 5',
+		completed: false,
+	},
+];
+
+app.get('/todos', (req, res) => res.send(todos));
+
+app.post('/todos', (req, res) => {
+	const todo = { title: req.body.title, id:Date.now(), completed: false };
+	todos.push(todo);
+	return res.send(todo);
+});
+
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	const index = todos.findIndex((todo) => todo.id == id);
+	const completed = Boolean(req.body.completed);
+	if (index > -1) {
+		todos[index].completed = completed;
+	}
+	return res.send(todos[index]);
+});
+
+app.delete('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	const index = todos.findIndex((todo) => todo.id == id);
+	if (index > -1) {
+		todos.splice(index, 1);
+	}
+
+	res.send(todos);
+});
+
+const PORT = 7000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
